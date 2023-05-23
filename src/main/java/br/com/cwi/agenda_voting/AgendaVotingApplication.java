a66060fba04c2007aecb74f955c5731616a8d742
Rafael Stefani Baptista <rafael.baptista@cwi.com.br>
2023-05-22 21:05:57 -0300

    Init project

---
+++ src/main/java/br/com/cwi/agenda_voting/AgendaVotingApplication.java
@@ -0,0 +1,13 @@
+package br.com.cwi.agenda_voting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaVotingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaVotingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cwi/agenda_voting/AgendaVotingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cwi.agenda_voting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaVotingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
