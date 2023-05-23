aa9faad4449489856cdfd8de3cc9cc2eae9edaa8
Rafael Stefani Baptista <rafael.baptista@cwi.com.br>
2023-05-23 16:20:32 -0300

    Minor changes in Agenda.

--- src/main/java/br/com/cwi/agenda_voting/domain/Agenda.java
+++ src/main/java/br/com/cwi/agenda_voting/domain/Agenda.java
@@ -1,5 +1,6 @@
 package br.com.cwi.agenda_voting.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -8,4 +9,10 @@ public class Agenda {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
     private Integer id;
+    @Column(name = "ID_CAMPO_TEXTO")
+    private String textField;
+    @Column(name = "ID_CAMPO_NUMERICO")
+    private String numericField;
+    @Column(name = "ID_CAMPO_DATA")
+    private String dateField;
 }
